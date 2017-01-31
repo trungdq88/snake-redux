9bfcbd3e965d407abeb8fa500b550915dd5b1c20
Trung Dinh Quang <trungdq88@gmail.com>
2017-01-31 14:50:09 +0700

    Clean up code

--- game.js
+++ game.js
@@ -1,27 +1,13 @@
-function pickLocation(snake) {
-  const position = () => ({
-    x: floor(random(BOARD_SIZE)),
-    y: floor(random(BOARD_SIZE)),
-  });
-
-  let newPosition = position();
-  while (snake.find(_ => newPosition.x === _.x && newPosition.y === _.y)) {
-    newPosition = position();
-  }
-  return newPosition;
-}
-
+// p5.js built-in method
 function setup() {
   createCanvas(BOARD_SIZE * SCALE, BOARD_SIZE * SCALE);
-  frameRate(40);
-
+  frameRate(SPEED);
   store.dispatch({ type: 'RESET' });
 }
 
+// p5.js built-in method
 function draw() {
-  const { snake, velocity, food, gameStart } = store.getState();
-
-  if (!gameStart) return;
+  const { snake, velocity, food } = store.getState();
 
   // Calculate new head
   let newX = snake[snake.length - 1].x + velocity.x;
@@ -31,8 +17,8 @@ function draw() {
   if (newY > BOARD_SIZE - 1) newY = 0;
   if (newY < 0) newY = BOARD_SIZE;
 
-  const head = { x: newX, y: newY };
-  const omnomnom = _ => dist(head.x, head.y, _.x, _.y) < 1;
+  const newHead = { x: newX, y: newY };
+  const omnomnom = target => newHead.x === target.x && newHead.y === target.y;
 
   // Dead?
   if (snake.find(omnomnom)) { // Did you just om..nom..nom your tail?
@@ -46,52 +32,66 @@ function draw() {
 
     store.dispatch({
       type: 'DROP_FOOD',
-      data: pickLocation(snake)
+      data: pickPositionThatDoesNotCollideWith(snake)
     });
   }
 
   // Move snake
-
   store.dispatch({
     type: 'MOVE_SNAKE',
-    data: head,
+    data: newHead,
   });
 
 }
 
+// p5.js built-in method
 function keyPressed() {
+  let newVelocity;
   if (keyCode === UP_ARROW) {
-    store.dispatch({
-      type: 'CHANGE_DIRECTION',
-      data: { x: 0, y: -1 }
-    });
+    newVelocity = { x: 0, y: -1 };
   } else if (keyCode === DOWN_ARROW) {
-    store.dispatch({
-      type: 'CHANGE_DIRECTION',
-      data: { x: 0, y: 1 }
-    });
+    newVelocity = { x: 0, y: 1 };
   } else if (keyCode === RIGHT_ARROW) {
-    store.dispatch({
-      type: 'CHANGE_DIRECTION',
-      data: { x: 1, y: 0 }
-    });
+    newVelocity = { x: 1, y: 0 };
   } else if (keyCode === LEFT_ARROW) {
-    store.dispatch({
-      type: 'CHANGE_DIRECTION',
-      data: { x: -1, y: 0 }
-    });
+    newVelocity = { x: -1, y: 0 };
+  } else {
+    return;
   }
+  store.dispatch({
+    type: 'CHANGE_DIRECTION',
+    data: newVelocity,
+  });
+}
+
+function pickPositionThatDoesNotCollideWith(arrayOfPosition) {
+  const position = () => ({
+    x: floor(random(BOARD_SIZE)),
+    y: floor(random(BOARD_SIZE)),
+  });
+
+  let pos = position();
+  while (arrayOfPosition.find(
+    target => pos.x === target.x && pos.y === target.y
+  )) {
+    pos = position();
+  }
+  return pos;
 }
 
 store.subscribe(_ => {
+  // Drow background
   background(50);
   const { snake, food } = store.getState();
+
+  // Draw snake
   fill(255);
   for (var i = 0; i < snake.length; i++) {
     fill(155 * (i / (snake.length - 1)) + 100);
     rect(snake[i].x * SCALE, snake[i].y * SCALE, SCALE, SCALE);
   }
 
+  // Draw food
   fill(255, 0, 25);
   rect(food.x * SCALE, food.y * SCALE, SCALE, SCALE);
 });
--- store.js
+++ store.js
@@ -1,21 +1,15 @@
-const SCALE = 10;
-const BOARD_SIZE = 60;
+const SCALE = 10;       // px per block
+const BOARD_SIZE = 60;  // blocks
+const SPEED = 40;       // blocks per second
 
 const INIT_STATE = {
   length: 1,
-  snake: [{
-    x: 0,
-    y: 0,
-  }],
-  velocity: {
-    x: 1,
-    y: 0,
-  },
-  food: {
+  snake: [{ x: 0, y: 0 }], // top left corner
+  velocity: { x: 0, y: 0 }, // standing by
+  food: { // first food placed in center of the board
     x: Math.floor(BOARD_SIZE / 2),
     y: Math.floor(BOARD_SIZE / 2),
   },
-  gameStart: false,
 };
 
 const reducer = (state = INIT_STATE, action) => {
@@ -23,45 +17,32 @@ const reducer = (state = INIT_STATE, action) => {
     case 'RESET':
       return INIT_STATE;
     case 'MOVE_SNAKE':
-      // New snake
+      // New snake => Add new position to the snake's head
       const newSnake = state.snake
         .concat(action.data)
+      // Cut the old tail based on length (take the last state.length elements)
         .slice(state.length * -1);
-
-      return Object.assign(
-        {}, state,
-        {
-          snake: newSnake,
-        }
-      );
+      return Object.assign({}, state, {
+        snake: newSnake,
+      });
     case 'CHANGE_DIRECTION':
-      return Object.assign(
-        {}, state,
-        {
-          velocity: action.data,
-          gameStart: true,
-        }
-      );
+      return Object.assign({}, state, {
+        velocity: action.data,
+      });
     case 'DROP_FOOD':
-      return Object.assign(
-        {}, state,
-        {
-          food: action.data,
-        }
-      );
+      return Object.assign({}, state, {
+        food: action.data,
+      });
     case 'GROW':
-      return Object.assign(
-        {}, state,
-        {
-          length: state.length + 1,
-        }
-      );
+      return Object.assign({}, state, {
+        length: state.length + 1,
+      });
     default:
       return state;
   };
-
 };
 
 const store = Redux.createStore(
-  reducer
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
