9fc5be59dc0a66bf2f1aafba0e0b3d6a829ab4ed
Trung Dinh Quang <trungdq88@gmail.com>
2017-01-31 16:11:49 +0700

    Step 1

--- README.md
+++
@@ -1,9 +0,0 @@
-# Snake Redux 🐍
-Some people think that Redux is **too complex** to add into their application compared to the benefits it brings back. Wrong. They might just feeling overwhelmed with bunch of tooling stuffs around Redux (and maybe with React).
-
-Here I am building a snake game using **only Redux**, no boilerplate, no build tool, no React/Webpact/Babel... to demostrate the real usage of Redux in a normal app.
-
-**Screencap:**
-
-![snake-redux](https://cloud.githubusercontent.com/assets/4214509/22457144/c72c1040-e7c8-11e6-926d-17cd0fd79ad5.gif)
-
--- game.js
+++ game.js
@@ -1,99 +1,26 @@
+const SCALE = 10; // px per block
+const BOARD_SIZE = 40; // blocks
+
 // p5.js built-in method
 function setup() {
   createCanvas(BOARD_SIZE * SCALE, BOARD_SIZE * SCALE);
-  frameRate(SPEED);
-  store.dispatch({ type: 'RESET' });
 }
 
-// p5.js built-in method
+// p5.js built-in method, called every frame (60 frames per second)
 function draw() {
-  const { snake, velocity, food } = store.getState();
-
-  // Calculate new head
-  let newX = snake[snake.length - 1].x + velocity.x;
-  let newY = snake[snake.length - 1].y + velocity.y;
-  if (newX > BOARD_SIZE - 1) newX = 0;
-  if (newX < 0) newX = BOARD_SIZE;
-  if (newY > BOARD_SIZE - 1) newY = 0;
-  if (newY < 0) newY = BOARD_SIZE;
-
-  const newHead = { x: newX, y: newY };
-
-  // This method check if the head is collide with `target`
-  const omnomnom = target => newHead.x === target.x && newHead.y === target.y;
-
-  // Dead?
-  if (snake.find(omnomnom)) { // Did you just om..nom..nom your tail?
-    store.dispatch({ type: 'RESET' });
-    return;
-  }
-
-  // Eat food
-  if (omnomnom(food)) {
-    store.dispatch({ type: 'GROW' });
-
-    store.dispatch({
-      type: 'DROP_FOOD',
-      data: pickPositionThatDoesNotCollideWith(snake)
-    });
-  }
-
-  // Move snake
-  store.dispatch({
-    type: 'MOVE_SNAKE',
-    data: newHead,
-  });
-
-}
-
-// p5.js built-in method
-function keyPressed() {
-  let newVelocity;
-  if (keyCode === UP_ARROW) {
-    newVelocity = { x: 0, y: -1 };
-  } else if (keyCode === DOWN_ARROW) {
-    newVelocity = { x: 0, y: 1 };
-  } else if (keyCode === RIGHT_ARROW) {
-    newVelocity = { x: 1, y: 0 };
-  } else if (keyCode === LEFT_ARROW) {
-    newVelocity = { x: -1, y: 0 };
-  } else {
-    return;
-  }
-  store.dispatch({
-    type: 'CHANGE_DIRECTION',
-    data: newVelocity,
-  });
-}
-
-function pickPositionThatDoesNotCollideWith(arrayOfPosition) {
-  const position = () => ({
-    x: floor(random(BOARD_SIZE)),
-    y: floor(random(BOARD_SIZE)),
-  });
-
-  let pos = position();
-  while (arrayOfPosition.find(
-    target => pos.x === target.x && pos.y === target.y
-  )) {
-    pos = position();
-  }
-  return pos;
-}
-
-store.subscribe(_ => {
   // Draw background
   background(50);
-  const { snake, food } = store.getState();
 
   // Draw snake
   fill(255);
-  for (var i = 0; i < snake.length; i++) {
-    fill(155 * (i / (snake.length - 1)) + 100);
-    rect(snake[i].x * SCALE, snake[i].y * SCALE, SCALE, SCALE);
-  }
+  rect(0, 0, SCALE, SCALE);
 
   // Draw food
   fill(255, 0, 25);
-  rect(food.x * SCALE, food.y * SCALE, SCALE, SCALE);
-});
+  rect(
+    Math.floor(BOARD_SIZE / 2) * SCALE,
+    Math.floor(BOARD_SIZE / 2) * SCALE,
+    SCALE,
+    SCALE
+  );
+}
